5c831b52efe4c36103a520f804a48ddb669ee9b5
Joseph Siddall <j.b.siddall@gmail.com>
2020-09-24 11:18:52 +0200

    fixed pack.pl

--- pack.pl
+++ pack.pl
@@ -1,5 +1,6 @@
-name("fastcsv").
-title("fastcsv").
-author("Joseph Siddall", "j.b.siddall@gmail.com").
-maintainer("Joseph Siddall", "j.b.siddall@gmail.com").
-home("https://github.com/jbsiddall/fastcsv").
+name(fastcsv).
+version('0.1.0').
+title('fastcsv').
+author('Joseph Siddall', 'j.b.siddall@gmail.com').
+maintainer('Joseph Siddall', 'j.b.siddall@gmail.com').
+home('https://github.com/jbsiddall/fastcsv').
